01f4b68f0df0d5f40ab35fb556cf832d4c5190a6
James Cotterill <jamescotterill93@gmail.com>
2023-06-03 20:31:29 +0100

    adding files to repo

---
+++ pythonProject2/helloworld1.py
@@ -0,0 +1,21 @@
+# print("Hello World")
+#
+# print('I am iron man')
+#
+# print('I am Tony Stark')
+#
+# print('I am poppy')
+
+# WE are practicing python
+
+# print("""I am Iron man.
+# No,I am Tony Stark.
+# No, I am Poppy.""")
+
+# print("I am Iron man. \n" + "No, I am ony Stark. \n" + "No, I am Poppy. \n")
+#
+# print('I am Poppy. \n' * 100)
+
+
+
+
